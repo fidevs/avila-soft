59e76fc8cf61fa8f62b852121bb518ab6a04e583
Fide Omar Islas Duarte <fidev.id@outlook.com>
2022-06-07 20:19:54 -0500

    Add login and register page

--- avila-soft-client/src/app/app-routing.module.ts
+++ avila-soft-client/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 ];
